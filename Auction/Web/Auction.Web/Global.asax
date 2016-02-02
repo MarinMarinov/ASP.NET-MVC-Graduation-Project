9e00a077216ae1657686ce494e39e0d1deaaa925
Marin Marinov <marin.telerik@gmail.com>
2016-02-02 06:41:47 +0200

    Project initialization

---
+++ Auction/Web/Auction.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Auction.Web.MvcApplication" Language="C#" %>
---
+++ Auction/Web/Auction.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auction.Web.Startup))]
+namespace Auction.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
