cf2737f130ae6585e73f3e580fe246245d2ebbab
Marin Marinov <marin.telerik@gmail.com>
2016-02-02 14:44:33 +0200

    Some fixes in UserDetails functionality

--- Auction/Web/Auction.Web/Controllers/UserController.cs
+++ Auction/Web/Auction.Web/Controllers/UserController.cs
@@ -2,16 +2,19 @@
 {
     using Models;
     using System.Linq;
+    using System.Runtime.Remoting.Contexts;
+    using System.Web;
     using System.Web.Mvc;
 
     public class UserController : Controller
     {
         public ApplicationDbContext db = new ApplicationDbContext();
 
-        public ActionResult UserDetails(string id)
+        public ActionResult UserDetails()
         {
+            var currentUserName = this.User.Identity.Name;
             var user =
-                this.db.Users.FirstOrDefault(u => u.UserName == id);
+                this.db.Users.FirstOrDefault(u => u.UserName == currentUserName);
 
             return View(user);
         }
