735e83decaa1851c0f54ca2b3874597267e43b02
Marin Marinov <marin.telerik@gmail.com>
2016-02-03 15:06:20 +0200

    Submit Html Helper added

---
+++ Auction/Web/Auction.Web/Infrastructure/Helpers/HtmlExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Auction.Web.Infrastructure.Helpers
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString Submit(this HtmlHelper helper, object htmlAttributes = null)
+        {
+            TagBuilder input = new TagBuilder("Input");
+            var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes) as IDictionary<string, object>;
+            input.MergeAttributes(attributes);
+            input.Attributes.Add("type", "submit");
+
+            return new MvcHtmlString(input.ToString());
+        }
+    }
+}
