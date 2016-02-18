248ca9e146873317b4dceaf6760cfbe76b1358b1
Marin Marinov <marin.telerik@gmail.com>
2016-02-18 17:01:24 +0200

    Custom Error page added

---
+++ Auction/Web/Auction.Web/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿namespace Auction.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+    }
+}
